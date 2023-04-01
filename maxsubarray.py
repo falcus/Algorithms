32c21f2df5e07ea21619b90a213abb28ad8e0493
falcus <crdeufel@gmail.com>
2023-04-01 16:15:55 +0200

    maxSubArray Problem

---
+++ maxsubarray.py
@@ -0,0 +1,15 @@
+#Give integer array "nums" find contiguous subarray containing at least one number which has the largest sum
+
+Input = [-2,1,-3,4,-1,2,1,-5,4]
+
+def maxSubArray(self, nums: List[int]) -> int:
+    # default max subarray is the first term of the array
+    maxSub = nums[0]
+    curSum = 0
+
+    for n in nums:
+        if curSum < 0:
+            curSum = 0
+        curSum += n
+        maxSub = max(maxSub, curSub)
+    return maxSub
